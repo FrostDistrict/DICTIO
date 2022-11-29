af998f280e661c71c2863061392cd3fc3a5d5722
Théo <thotim.de.verdiere@gmail.com>
2022-11-29 11:25:32 -0500

    init

--- src/swing/Dictionnaire.java
+++ src/swing/Dictionnaire.java
@@ -4,15 +4,18 @@ import javax.swing.*;
 
 public class Dictionnaire {
     private JPanel panel1;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
+    private JButton buttonAdd;
+    private JButton buttonRegister;
+    private JButton buttonCharge;
     private JTextField entrezUnMotTextField;
     private JList list1;
     private JTextArea textArea1;
     private JList list2;
+    private JScrollBar scrollBar1;
 
     public Dictionnaire() {
 
     }
+
+
 }
