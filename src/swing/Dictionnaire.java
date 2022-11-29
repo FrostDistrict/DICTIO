c6524205bd1542ac9781d85becdfaea805a4743d
Théo <thotim.de.verdiere@gmail.com>
2022-11-29 10:42:21 -0500

    init

--- src/swing/Dictionnaire.java
+++ src/swing/Dictionnaire.java
@@ -1,4 +1,18 @@
 package swing;
 
+import javax.swing.*;
+
 public class Dictionnaire {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JTextField entrezUnMotTextField;
+    private JList list1;
+    private JTextArea textArea1;
+    private JList list2;
+
+    public Dictionnaire() {
+
+    }
 }
